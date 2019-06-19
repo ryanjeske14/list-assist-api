9feb74a5e930fb936437924ac306293715e5526e
Ryan Jeske <ryanjeske@gmail.com>
2019-06-19 00:18:45 -0600

    initial implementation of recipes-post-endpoint

--- src/recipes/recipes-router.js
+++ src/recipes/recipes-router.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const path = require("path");
 const RecipesService = require("./recipes-service");
+const { requireAuth } = require("../middleware/jwt-auth");
 
 const recipesRouter = express.Router();
+const jsonBodyParser = express.json();
 
 recipesRouter.route("/").get((req, res, next) => {
   RecipesService.getAllRecipes(req.app.get("db"))
@@ -9,6 +12,64 @@ recipesRouter.route("/").get((req, res, next) => {
       if (recipes.rows[0].json_agg != null) {
         res.json(recipes.rows[0].json_agg);
       } else res.json([]);
+    })
+    .catch(next);
+});
+
+recipesRouter.route("/test").get((req, res, next) => {
+  RecipesService.getById(req.app.get("db"), 2)
+    .then(recipe => {
+      if (recipe.rows[0] != null) {
+        res.json(recipe.rows[0]);
+      } else res.json({});
+    })
+    .catch(next);
+});
+
+recipesRouter.route("/").post(jsonBodyParser, (req, res, next) => {
+  const recipe = req.body;
+  const ingredients = recipe.ingredients.map(ingredient => {
+    return { name: ingredient.name };
+  });
+  const newRecipe = {
+    name: recipe.name,
+    description: recipe.description,
+    instructions: recipe.instructions
+  };
+  const recipeIngredients = recipe.ingredients.map(ingredient => {
+    return {
+      name: ingredient.name,
+      quantity: ingredient.quantity,
+      unit_id: 3,
+      special_instructions: ingredient.special_instructions
+    };
+  });
+
+  for (const [key, value] of Object.entries(recipe))
+    if (value == null)
+      return res.status(400).json({
+        error: `Missing '${key}' in request body`
+      });
+
+  //newRecipe.owner_id = req.user.id;
+  newRecipe.owner_id = 1;
+
+  RecipesService.insertRecipe(
+    req.app.get("db"),
+    ingredients,
+    newRecipe,
+    recipeIngredients
+  )
+    .then(result => {
+      if (result.rows[0] != null) {
+        return result.rows[0];
+      } else return {};
+    })
+    .then(recipe => {
+      res
+        .status(201)
+        .location(path.posix.join(req.originalUrl, `/${recipe.id}`))
+        .json(recipe);
     })
     .catch(next);
 });
--- src/recipes/recipes-service.js
+++ src/recipes/recipes-service.js
@@ -13,6 +13,56 @@ const RecipesService = {
             ) as ingredients
             from recipes as r) as rec
         `);
+  },
+
+  getById(db, id) {
+    return db.raw(`select r.id, r.name, r.description, r.instructions,
+    (select json_agg(recing)
+    from (
+      select ingredient_id as id, i.name, quantity, u.name as unit, special_instructions, recipe_id from recipes_ingredients 
+      join ingredients i on ingredient_id = i.id
+      join units u on unit_id = u.id
+      where recipe_id = r.id
+      ) recing
+    ) as ingredients
+    from recipes r
+    where r.id = ${id}`);
+  },
+
+  insertRecipe(db, ingredients, newRecipe, recipeIngredients) {
+    let newRecipeId;
+    return db
+      .transaction(async trx => {
+        await trx
+          .into("ingredients")
+          .insert(ingredients)
+          .returning("*")
+          .then(ingredients => {
+            for (let recipeIngredient of recipeIngredients) {
+              for (let ingredient of ingredients) {
+                if (recipeIngredient.name === ingredient.name) {
+                  recipeIngredient.ingredient_id = ingredient.id;
+                  delete recipeIngredient.name;
+                }
+              }
+            }
+          });
+        await trx
+          .into("recipes")
+          .insert(newRecipe)
+          .returning("id")
+          .then(([recipeId]) => recipeId)
+          .then(recipeId => {
+            for (let recipeIngredient of recipeIngredients) {
+              recipeIngredient.recipe_id = recipeId;
+            }
+            newRecipeId = recipeId;
+          });
+        await trx.into("recipes_ingredients").insert(recipeIngredients);
+      })
+      .then(() => {
+        return RecipesService.getById(db, newRecipeId);
+      });
   }
 };
 
